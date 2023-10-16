9c3b44a34e0d5d5dcf7b6dcbb91feca6779df7cb
zoz24 <zizhanzhou24@gmail.com>
2023-10-16 18:49:58 -0500

    3a done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+app.use(morgan("tiny"));
+morgan.token("body", (req, res) => JSON.stringify(req.body));
 
 let persons = [
   {
